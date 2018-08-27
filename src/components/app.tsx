f6c553e5fd3e720fd38f610426651f206c54d5c1
rinsuki <428rinsuki+git@gmail.com>
2018-08-27 09:28:11 +0900

    first

---
+++ src/components/app.tsx
@@ -0,0 +1,21 @@
+import React = require("react")
+
+interface Props {
+}
+
+interface State {
+}
+
+export class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return <div>
+            hello
+        </div>
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React = require("react")
+import ReactDOM = require("react-dom")
+import { App } from "./components/app"
+
+const root = document.getElementById("app")
+ReactDOM.render(<App />, root)
